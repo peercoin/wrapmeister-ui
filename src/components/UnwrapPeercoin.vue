9aeb3f1399aa91eda421abfeb87687d2064ab3ca
Peerchemist <dx.1178@gmail.com>
2021-09-03 11:18:13 +0200

    10

--- src/components/UnwrapPeercoin.vue
+++ src/components/UnwrapPeercoin.vue
@@ -51,7 +51,7 @@
           @keypress="onlyForCurrency"
         />
         <p v-if="minAmountNotExceeded" class="text-danger text-end fs-6">
-          Minimum amount is set at 1000
+          Minimum amount is set at 10
         </p>
       </div>
     </div>
