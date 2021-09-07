507fa1ef4942d9eede3e5413c2afcf6338f2df82
Po <counterparty@outlook.com>
2021-09-07 17:53:25 +0200

    Dynamic titels

--- src/Endpoints.js
+++ src/Endpoints.js
@@ -3,11 +3,6 @@ const APP_WRAPPERHOST = "https://wrapmeister.peercoin.net";
 const APP_WRAPPERHOSTWSS = "ws://localhost:3000"; 
 
 const isNullOrEmpty = function(value) {
-
-
-
-//trigger
-
   return !(typeof value === "string" && value.length > 0);
 };
 
@@ -55,36 +50,42 @@ export function getNetworks() {
       description: "Binance Smart Chain (BSC) - Testnet",
       active: false,
       contract: "0xd11C56F90481062D3e99eA72Df816Dc768BaAeE1",
+      pagetitle: "Peercoin ↔ Ethereum Bridge"
     },
     {
       key: "BSC_MAINNET",
       description: "Binance Smart Chain (BSC)",
       active: false,
       contract: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+      pagetitle: "Peercoin ↔ Ethereum Bridge"
     },
     {
       key: "MATIC_TESTNET",
       description: "Polygon (Matic) - Testnet",
       active: false,
       contract: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+      pagetitle: "Peercoin ↔ Ethereum Bridge"
     },
     {
       key: "MATIC_MAINNET",
       description: "Polygon (Matic)",
       active: false,
       contract: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+      pagetitle: "Peercoin ↔ Ethereum Bridge"
     },
     {
       key: "ETH_TESTNET",
       description: "Ethereum (ETH) - Testnet",
       active: true,
       contract: "0x5B8eF6EE9ec9Df9f240FEbACa2AE88ce3eb950dc",
+      pagetitle: "Peercoin ↔ Ethereum Bridge"
     },
     {
       key: "ETH_MAINNET",
       description: "Ethereum (ETH)",
       active: false,
       contract: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+      pagetitle: "Peercoin ↔ Ethereum Bridge"
     },
   ];
 }
--- src/components/WrapHeader.vue
+++ src/components/WrapHeader.vue
@@ -4,10 +4,31 @@
       <img alt="Peercoin" height="75" src="../assets/logo-white.svg" />
     </div>
     <div class="col-xs-12 col-lg-10">
-    <p class="page-title">Peercoin ↔ Ethereum Bridge</p>
+      <p class="page-title">{{ curSelectedTitle }}</p>
     </div>
   </div>
 </template>
+
+<script>
+import { getNetworks } from "@/Endpoints.js";
+
+export default {
+  computed: {
+    curSelectedTitle() {
+      let network = this.$store.state.network;
+      console.log(network)
+      if (!!network) {
+        const networkdetail = getNetworks().find(
+          (nw) => nw.key === network
+        );
+
+        return !!networkdetail ? networkdetail.pagetitle : "";
+      }
+      return "";
+    },
+  },
+};
+</script>
 
 <style lang="scss" scoped>
 .page-title {
