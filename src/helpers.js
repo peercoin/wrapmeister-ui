aed34fddd5a0a192b5ab267a1899d76d887ba09d
Po <counterparty@outlook.com>
2022-01-31 07:51:52 +0100

    amount not correctly rounded

--- src/helpers.js
+++ src/helpers.js
@@ -1,11 +1,16 @@
-export function debounce (fn, delay) {
-    var timeoutID = null
-    return function () {
-      clearTimeout(timeoutID)
-      var args = arguments
-      var that = this
-      timeoutID = setTimeout(function () {
-        fn.apply(that, args)
-      }, delay)
-    }
-  }
+export function debounce(fn, delay) {
+  var timeoutID = null;
+  return function() {
+    clearTimeout(timeoutID);
+    var args = arguments;
+    var that = this;
+    timeoutID = setTimeout(function() {
+      fn.apply(that, args);
+    }, delay);
+  };
+}
+
+export function roundTo(x, digits) {
+  const power = con(10 ** digits);
+  return Math.round((x + Number.EPSILON) * power) / power;
+}
