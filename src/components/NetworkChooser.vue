e5f50bdc17aca09c84b530d85ca26a2876720aa2
Po <counterparty@outlook.com>
2022-02-05 21:12:08 +0100

    custom caret + readable text in dropdown

--- src/components/NetworkChooser.vue
+++ src/components/NetworkChooser.vue
@@ -4,6 +4,7 @@
     v-model="curSelected"
     label="network"
     track-by="network"
+    :searchable="false"
     :options="availableNetworks"
     :option-height="104"
     placeholder="Select smart contract platform"
@@ -13,6 +14,17 @@
     :allow-empty="false"
     @select="onOptionSelectChange"
   >
+    <template v-slot:caret="slotProps">
+      <span class="arrowcaret">
+        <img
+          alt="Peercoin"
+          height="7"
+          src="../assets/arrowdown.svg"
+          @mousedown.prevent.stop="slotProps.toggle"
+        />
+      </span>
+    </template>
+
     <template v-slot:singleLabel="slotProps">
       <img
         v-if="!!slotProps && !!slotProps.option && !!slotProps.option.img"
@@ -128,6 +140,8 @@ export default {
   font-size: 14px;
   padding: 6px 12px;
   min-height: 20px;
+  color: #2f2f2f;
+  background-color: #3cb054;
 }
 
 .dropdown-ppc-style .multiselect__option--highlight {
@@ -237,28 +251,32 @@ export default {
 }
 
 .dropdown-ppc-style .multiselect__option--selected {
-  background: #cccccc;
-  color: #4e4e4e;
+  background: #3cb054;
+  color: #fff;
 }
 
 .dropdown-ppc-style
   .multiselect__option--selected.multiselect__option--highlight {
-  background: #3cb054;
+  background-color: #ade2b7;
+  color: #0a0909;
 }
 
 .dropdown-ppc-style
   .multiselect__option--selected.multiselect__option--highlight:after {
-  background: #3cb054;
+  background-color: #ade2b7;
+  color: #0a0909;
 }
 
 .dropdown-ppc-style
   .multiselect__option--group-selected.multiselect__option--highlight {
-  background: #3cb054;
+  background-color: #ade2b7;
+  color: #0a0909;
 }
 
 .dropdown-ppc-style
   .multiselect__option--group-selected.multiselect__option--highlight:after {
-  background: #3cb054;
+  background-color: #ade2b7;
+  color: #0a0909;
 }
 .dropdown-ppc-style .multiselect__placeholder {
   color: inherit;
@@ -292,10 +310,20 @@ export default {
   background: #3cb054;
   //font-size: 14px;
 }
-.dropdown-ppc-style  span.option__title {
-    color: #fff;
+.dropdown-ppc-style span.option__title {
+  color: #fff;
+  //background-color: #061988;
 }
- 
+.arrowcaret {
+  position: absolute;
+  width: 40px;
+  height: 38px;
+  right: 1px;
+  top: 1px;
+  padding: 4px 8px;
+  text-align: center;
+  transition: transform 0.2s ease;
+}
 </style>
 
 <style lang="scss" scoped>
@@ -322,6 +350,8 @@ export default {
   padding-top: 5px;
   text-align: left;
   width: 100%;
-   color: #fff;
+  color: #fff;
+  //background-color: #7c2d14;
 }
+//multiselect__option--highlight multiselect__option--selected multiselect__option
 </style>
